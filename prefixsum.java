b153d8e878154a3dd9aa8332c18bfc3c12a74e1f
Bhavyasri837 <nsbhavya83@gmail.com>
2024-12-09 09:50:36 +0530

    Add files via upload

---
+++ prefixsum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class prefixsum {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        int pre[]=new int[n];
+        pre[0]=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            pre[i]=arr[i]+pre[i-1];
+        }
+        System.out.println(Arrays.toString(pre));
+    }
+    
+}
